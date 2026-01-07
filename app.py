b3756e15478d2a0035955a8fe2e8dddc9d734d34
lidorg-dev <39281113+lidorg-dev@users.noreply.github.com>
2026-01-07 19:11:49 +0200

    Update greeting message in root route

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def elapsed():
 
 @app.route('/')
 def root():
-    return "Hello World -new version 3.00 BY Lidor (Python)! (up %s)\n" % elapsed()
+    return "Hello World -new version 3.00 BY Lidor -DEVOPS (Python)! (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
